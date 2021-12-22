cc9e772c3a61b74656c084d1750c3bcf3437ed05
Лузин Семён <Cemen.Luzin@yandex.ru>
2021-12-23 04:44:13 +0700

    не работает: добавление девайса исправлено: окончательно вырезан рейтинг

--- server/models/models.js
+++ server/models/models.js
@@ -20,7 +20,6 @@ const Device = sequelize.define('device', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     name: {type: DataTypes.STRING, unique: true, allowNull: false},
     price: {type: DataTypes.INTEGER, allowNull: false},
-    rating: {type: DataTypes.INTEGER, defaultValue: 0},
     img: {type: DataTypes.STRING, allowNull: false},
 })
 
